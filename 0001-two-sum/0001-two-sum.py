4708694813b199d3caad6e5e4fa069775f5b2030
Tyler Huynh <142545753+tyhhuynh@users.noreply.github.com>
2025-06-23 14:01:39 -0700

    Time: 1 ms (65.95%), Space: 13.4 MB (15.66%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,13 +1,16 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
-        return []
         """
         :type nums: List[int]
         :type target: int
         :rtype: List[int]
         """
-        
+        hash = {}
+
+        for i in range(len(nums)):
+            hash[nums[i]] = i
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hash and hash[complement] != i:
+                return [i, hash[complement]]
+        return []
