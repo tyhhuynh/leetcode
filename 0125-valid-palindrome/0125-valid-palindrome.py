1e9db09b641d9128d90d9a59e99854a5b4cddd65
Tyler Huynh <142545753+tyhhuynh@users.noreply.github.com>
2025-06-25 12:49:56 -0700

    Time: 11 ms (36.21%), Space: 18.1 MB (56.6%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,24 +1,22 @@
-class Solution(object):
-    def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        l, r = 0, len(s) - 1
-        while l < r:
-            while l < r and not self.alphanum(s[l]):
-                l += 1
-            while l < r and not self.alphanum(s[r]):
-                r -= 1
-            if s[l].lower() != s[r].lower():
-                return False
-            l += 1
-            r -= 1
-        return True
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left, right = 0, len(s)-1
 
-    def alphanum(self, c):
-        return (
-            ord("A") <= ord(c) <= ord("Z")
-            or ord("a") <= ord(c) <= ord("z")
-            or ord("0") <= ord(c) <= ord("9")
-        )
+        while left < right:
+
+            # checks to see if element in s is alphanumeric
+            if not s[left].isalnum():
+                left += 1
+                continue
+            if not s[right].isalnum():
+                right -= 1
+                continue
+            
+            # checks if left and right pointers are !equal
+            if s[left].lower() != s[right].lower():
+                return False
+
+            left += 1
+            right -= 1
+            
+        return True
