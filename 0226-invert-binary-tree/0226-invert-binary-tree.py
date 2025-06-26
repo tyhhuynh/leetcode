c82846bb2491cac5533478e2094a64ef0ad71d1d
Tyler Huynh <142545753+tyhhuynh@users.noreply.github.com>
2025-06-26 12:18:21 -0700

    Time: 0 ms (100%), Space: 17.9 MB (8.88%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.py
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -6,11 +6,20 @@
 #         self.right = right
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        if root is None:
+        if root is None:  # base case: null node
             return None
+
+        # recursive case: recursively invert left & right subtrees
         l_inverted = self.invertTree(root.left)
         r_inverted = self.invertTree(root.right)
 
+        # swapping left and right child nodes
         root.left = r_inverted
         root.right = l_inverted
+
+        # returns current subtree
         return root
+
+        # final notes:
+        # DFS -> traverses all the way down to leaves
+        # bottom-up -> swaps child nodes after being processed by DFS
