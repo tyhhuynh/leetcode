bb2f1f97e7f7e50650e93e283b7bcd4ada152240
Tyler Huynh <142545753+tyhhuynh@users.noreply.github.com>
2025-06-26 12:20:06 -0700

    Time: 0 ms (100%), Space: 17.8 MB (80.43%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,20 +1,19 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
+class Solution:
+    def isValid(self, s: str) -> bool:
+
         stack = []
         closeToOpen = {
-        ")": "(",
-        "]": "[",
-        "}": "{"}
+            ")": "(",
+            "]": "[",
+            "}": "{"
+        }
+
         for c in s:
-            if c in closeToOpen:
-                if stack and stack[-1] == closeToOpen[c]:
+            if c in closeToOpen: # closing brackets
+                if stack and stack[-1] == closeToOpen[c]: # stack !empty && top of stack matches opening bracket for c
                     stack.pop()
                 else:
                     return False
-            else:
+            else: # opening brackets
                 stack.append(c)
         return True if not stack else False
