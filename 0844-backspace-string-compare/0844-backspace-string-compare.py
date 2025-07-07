bdea7bd5e74675ac1ca490770079b08b6a179d59
Tyler Huynh <142545753+tyhhuynh@users.noreply.github.com>
2025-07-07 14:28:16 -0700

    Time: 3 ms (13.53%), Space: 17.8 MB (57.74%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.py
+++ 0844-backspace-string-compare/0844-backspace-string-compare.py
@@ -10,20 +10,24 @@ class Solution:
                     skip += 1
                 elif skip > 0:
                     skip -= 1
-                else:
-                    break
-                index -= 1
-            return index
+                else: # skip == 0 & s[index] != "#"
+                    break # break out of while loop and return index
+                index -= 1 # applies "backspace" if skip == 1
+            return index # index = index of next valid char
         
         while i >= 0 or j >= 0:
             i = backspace_char(s, i)
             j = backspace_char(t, j)
 
+            # both strings were exhausted: length of strings are equal & chars match
             if i < 0 and j < 0:
                 return True
+            
+            # length of strings are not equal || char of strings do not match
             if i < 0 or j < 0 or s[i] != t[j]:
                 return False
 
             i -= 1
             j -= 1
+
         return True
