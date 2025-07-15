ac4b23e2f5dd28faa64a3d608efc6b4e3ee26e88
Tyler Huynh <142545753+tyhhuynh@users.noreply.github.com>
2025-07-15 12:04:08 -0700

    Time: 7 ms (98.48%), Space: 12.6 MB (9.64%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -4,10 +4,12 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        if n <= 0:
-            return n
+        # base case:
         a, b = 0, 1
+        if n <= 0:
+            return a
+
+        # recursive case:
         for i in range(2, n + 1):
             a, b = b, a + b
         return b
-
