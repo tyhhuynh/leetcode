bf11c3849e1ce91ecdf20954da2638c0bb54f5cc
Tyler Huynh <142545753+tyhhuynh@users.noreply.github.com>
2025-11-09 12:39:09 -0800

    updated solution to Two Sum

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,6 +1,22 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(0, len(nums)-1):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+
+        # brute force: O(n^2)
+        # for i in range(0, len(nums) - 1):
+        #     for j in range(i + 1, len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return [i, j]
+
+        # hashmap solution: O(n)
+        hash = {}
+        
+        for i in range(len(nums)):
+            hash[nums[i]] = i # {element: index}
+            
+
+        for i in range(len(nums)):
+            complement = target - nums[i] # target - element
+            if complement in hash and hash[complement] != i: # complement exists as element in hash & its index is not duplicate
+                return [i, hash[complement]]
+        return []
+        
